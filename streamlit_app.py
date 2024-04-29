fcb04ea53273e30dc580eeeee6ac545da5e72d97
Hyago Vieira <70180407+HyAgOsK@users.noreply.github.com>
2024-04-29 05:20:28 +0000

    saoiudi

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ def video_input(data_src):
             output_img, class_name, _ = infer_image(frame)
             output.image(output_img)
             curr_time = time.time()
-            for i in (0, curr_time - prev_time < 10):
+            for i in (0, curr_time - prev_time < 60):
                 fps = fps + 1 / (curr_time - prev_time)
             fps = fps/10
             prev_time = curr_time
